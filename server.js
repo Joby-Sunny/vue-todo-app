5d16bfe40444e7e98500b8f6c93737c0cfd0ff28
Joby-Sunny <jobys39@live.com>
2019-12-31 18:55:58 +0530

    Server File : server file updated

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + "/dist"));
 
 app.use(function(req, res, next) {
- res.header("Access-Control-Allow-Origin", "*");
- res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
- next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
-app.get('/*', function(req, res) {
- res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8000);
